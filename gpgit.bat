9316dc76f789bd103ee67f8f29e77d43446376bc
Tinram <Tinram@users.noreply.github.com>
2018-04-06 15:05:11 +0100

    update gpgit.bat

--- gpgit.bat
+++ gpgit.bat
@@ -1,21 +1,27 @@
 
 @echo off
 
-REM GPGit - Windows batch script to strengthen the symmetric encryption of a file by GnuPG
+REM GPGit
+REM
+REM Windows batch script to strengthen symmetric file encryption by GnuPG.
 REM
 REM Usage:      gpgit <filename>
 REM Author:     Martin Latter, 23/03/2016, 17/11/2016
-REM Version:    0.12
+REM Version:    0.13
 REM License:    GNU GPL version 3.0 (GPL v3); http://www.gnu.org/licenses/gpl.html
 REM Link:       https://github.com/Tinram/GPGit.git
 
 
 if "%~1"=="" goto fnabort
+if "%1"=="-c" goto fnabort
 
-gpg -c --s2k-mode 3 --s2k-count 65011712 --s2k-digest-algo SHA512 --cipher-algo AES256 %1
+gpg -c --s2k-mode 3 --s2k-count 65011712 --s2k-digest-algo SHA512 --cipher-algo AES256 --require-secmem -z 9 %1
 
 REM cascade encryption: two passphrases required, take the resulting <filename>.gpg.gpg file
-if "%~2"=="-c" gpg -c --s2k-mode 3 --s2k-count 65011712 --s2k-digest-algo SHA512 --cipher-algo CAMELLIA256 %1.gpg
+if "%~2"=="-c" (
+    echo applying cascade encryption: second password required ...
+    gpg -c --s2k-mode 3 --s2k-count 65011712 --s2k-digest-algo SHA512 --cipher-algo CAMELLIA256 --require-secmem -z 9 %1.gpg
+)
 
 goto end
 
